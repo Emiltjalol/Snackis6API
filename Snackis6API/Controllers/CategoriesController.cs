76e5c4030e2654a225100706a81687eb20d28ab2
Emiltjalol <125493876+Emiltjalol@users.noreply.github.com>
2024-06-06 13:31:27 +0200

    Add project files.

---
+++ Snackis6API/Controllers/CategoriesController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Snackis6API.Data;
+using Snackis6API.Models;
+using static Snackis6API.Data.Snackis6Context;
+
+namespace Snackis6API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly Snackis6Context _context;
+
+        public CategoriesController(Snackis6Context context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Category>>> GetCategories()
+        {
+            return await _context.Category.ToListAsync();
+        }
+    }
+}
---
+++ Snackis6API/Data/UserDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Snackis6API.Models;
+
+namespace Snackis6API.Data
+{
+    public class Snackis6Context : DbContext
+    {
+        public Snackis6Context(DbContextOptions<Snackis6Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Category> Category { get; set; }
+    }
+}
